7bfd28919de50a017c51d12cd857112b5b661a81
Julian Romero <x0programmerx0@gmail.com>
2021-05-12 03:01:22 +1000

    Connected to a Library https://smtpjs.com/ to send Emals through the CONTACT FORM

---
+++ JS/contact.js
@@ -0,0 +1,38 @@
+
+// Listen for a Submit
+document.querySelector(".contact-form").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  // Get input Values
+  let name = document.querySelector(".name").nodeValue;
+  let email = document.querySelector(".email").nodeValue;
+  let message = document.querySelector(".message").nodeValue;
+
+  // saveContactInfo(name, email, message);
+
+  document.querySelector(".contact-form").requestFullscreen();
+
+  sendEmail(name, email, message);
+}
+
+// Send Email to me
+function sendEmail(name, email, message) {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: 'julianchorom@gmail.com',
+    Password: "zsrjweidodcpilmv",
+    To: 'julianchorom@gmail.com',
+    From: 'julianchorom@gmail.com',
+    Subject: `${name} sent you a message`,
+    Body: `Name: ${name} <br/> Email ${email} <br/> Message: ${message}`,
+  }).then((message) => alert("mail send successfully"))
+}
+
+document.querySelector(".button").addEventListener("click", trigger);
+
+function trigger() {
+  console.log("Holi amigo");
+  // alert("fuck mate")
+}
