7875aacf84bb01fbe93aee7101632b712d71401c
Julian Romero <x0programmerx0@gmail.com>
2021-05-14 17:51:53 +1000

    CONTACT FORM WORKING NICELY, error fixed

--- JS/contact.js
+++ JS/contact.js
@@ -33,6 +33,12 @@ function sendEmail(name, email, message) {
 document.querySelector(".button").addEventListener("click", trigger);
 
 function trigger() {
-  console.log("Holi amigo");
-  // alert("fuck mate")
+  let name = document.querySelector(".name").value;
+  let email = document.querySelector(".email").value;
+  let message = document.querySelector(".message").value;
+  console.log(name);
+  console.log(email);
+  console.log(message);
+  console.log("Hola amigo");
+  // alert("mate")
 }
